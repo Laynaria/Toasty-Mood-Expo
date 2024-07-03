cbc1cbafef17eceff44636cbe2d4cff73c555a98
Laynaria <pro.laynaria@outlook.fr>
2024-07-03 18:06:02 +0200

    clicked icon now change in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,45 @@
-import { Link } from "expo-router";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Link, usePathname } from "expo-router";
+import { Image, Pressable, StyleSheet, View } from "react-native";
 
-const homeIcon = require("../assets/icons/home-icon.png");
+const homeIcon = require("../assets/icons/home.png");
 const timelineIcon = require("../assets/icons/timeline.png");
 const shopIcon = require("../assets/icons/shop.png");
 const settingsIcon = require("../assets/icons/settings.png");
 
+const homeClicked = require("../assets/icons/home-clicked.png");
+const timelineClicked = require("../assets/icons/timeline-clicked.png");
+const shopClicked = require("../assets/icons/shop-clicked.png");
+const settingsClicked = require("../assets/icons/settings-clicked.png");
+
 export default function Navbar() {
+  const path = usePathname();
   return (
     <View style={styles.container}>
       <Link href="/" asChild style={styles.link}>
         <Pressable>
-          <Image source={homeIcon} />
+          <Image source={path === "/" ? homeClicked : homeIcon} />
         </Pressable>
       </Link>
 
       <Link href="/timeline" asChild style={styles.link}>
         <Pressable>
-          <Image source={timelineIcon} />
+          <Image
+            source={path === "/timeline" ? timelineClicked : timelineIcon}
+          />
         </Pressable>
       </Link>
 
       <Link href="/shop" asChild style={styles.link}>
         <Pressable>
-          <Image source={shopIcon} />
+          <Image source={path === "/shop" ? shopClicked : shopIcon} />
         </Pressable>
       </Link>
 
       <Link href="/settings" asChild style={styles.link}>
         <Pressable>
-          <Image source={settingsIcon} />
+          <Image
+            source={path === "/settings" ? settingsClicked : settingsIcon}
+          />
         </Pressable>
       </Link>
     </View>
