4143ed70acbbfe5b4704814c6a2e36226205d077
Laynaria <pro.laynaria@outlook.fr>
2024-07-03 16:21:32 +0200

    added expo router

--- app/timeline.tsx
+++ app/timeline.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Timeline() {
   return (
     <View style={styles.container}>
-      <Text>Toasty Mood Test!</Text>
+      <Text>Toasty Mood Timeline Page!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
