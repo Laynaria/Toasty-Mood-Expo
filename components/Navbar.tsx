01b459268465911577445774fc9384b71f061502
Laynaria <pro.laynaria@outlook.fr>
2024-07-03 17:41:38 +0200

    navbar basic design done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,51 @@
 import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+const homeIcon = require("../assets/icons/home-icon.png");
+const timelineIcon = require("../assets/icons/timeline.png");
+const shopIcon = require("../assets/icons/shop.png");
+const settingsIcon = require("../assets/icons/settings.png");
 
 export default function Navbar() {
   return (
-    <View>
-      <Link href="/" asChild>
+    <View style={styles.container}>
+      <Link href="/" asChild style={styles.link}>
         <Pressable>
-          <Text>Home</Text>
+          <Image source={homeIcon} />
         </Pressable>
       </Link>
 
-      <Link href="/timeline" asChild>
+      <Link href="/timeline" asChild style={styles.link}>
         <Pressable>
-          <Text>Timeline</Text>
+          <Image source={timelineIcon} />
         </Pressable>
       </Link>
 
-      <Link href="/shop" asChild>
+      <Link href="/shop" asChild style={styles.link}>
         <Pressable>
-          <Text>Shop</Text>
+          <Image source={shopIcon} />
         </Pressable>
       </Link>
 
-      <Link href="/settings" asChild>
+      <Link href="/settings" asChild style={styles.link}>
         <Pressable>
-          <Text>Settings</Text>
+          <Image source={settingsIcon} />
         </Pressable>
       </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#E3A062",
+    paddingVertical: 10,
+  },
+  link: {
+    padding: 15,
+  },
+});
