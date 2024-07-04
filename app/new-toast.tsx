f081ba62f502393625e8451942e2ae08cf4099c5
Laynaria <pro.laynaria@outlook.fr>
2024-07-04 12:42:14 +0200

    added some style to newToast page

--- app/new-toast.tsx
+++ app/new-toast.tsx
@@ -1,5 +1,15 @@
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
+
+const bgImg = require("../assets/background-toasts-flip.png");
 
 export default function NewToast() {
   const date = new Date();
@@ -21,12 +31,27 @@ export default function NewToast() {
 
   const daySuffix = ["st", "nd", "rd"];
 
+  const handleSubmit = () => {
+    router.back();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{`${months[date.getMonth()]} ${date.getDay()}${
-        date.getDay() > 3 ? "th" : daySuffix[date.getDay() - 1]
-      }`}</Text>
-      <StatusBar style="auto" />
+      <View style={styles.subContainer}>
+        <Text style={styles.text}>{`${
+          months[date.getMonth()]
+        } ${date.getDay()}${
+          date.getDay() > 3 ? "th" : daySuffix[date.getDay() - 1]
+        }`}</Text>
+        <Text style={styles.text}>How was your day today?</Text>
+        <TextInput multiline placeholder="Today's Note" />
+
+        <Pressable style={styles.doneButton} onPress={handleSubmit}>
+          <Text style={[styles.text, { color: "white" }]}>I'm Done!</Text>
+        </Pressable>
+        <StatusBar style="auto" />
+      </View>
+      <Image source={bgImg} style={styles.background} />
     </View>
   );
 }
@@ -34,8 +59,34 @@ export default function NewToast() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 80,
+  },
+  subContainer: {
+    backgroundColor: "#fff",
+    width: "70%",
+    height: "100%",
+    gap: 10,
+    justifyContent: "flex-start",
+  },
+
+  text: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#6A3C11",
+  },
+
+  doneButton: {
+    backgroundColor: "#E3A062",
+    width: "100%",
+    padding: 12,
+    borderRadius: 12,
+  },
+
+  background: {
+    position: "absolute",
+    width: "100%",
+    bottom: 0,
   },
 });
