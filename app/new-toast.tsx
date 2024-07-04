9a6a6b9e94bbd0e55c15b4b8d3b8288cc5d6da74
Laynaria <pro.laynaria@outlook.fr>
2024-07-04 12:05:36 +0200

    algorithm to write date on new toast page done

--- app/new-toast.tsx
+++ app/new-toast.tsx
@@ -2,9 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function NewToast() {
+  const date = new Date();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const daySuffix = ["st", "nd", "rd"];
+
   return (
     <View style={styles.container}>
-      <Text>Toasty Mood Add Toast Page!!!</Text>
+      <Text>{`${months[date.getMonth()]} ${date.getDay()}${
+        date.getDay() > 3 ? "th" : daySuffix[date.getDay() - 1]
+      }`}</Text>
       <StatusBar style="auto" />
     </View>
   );
