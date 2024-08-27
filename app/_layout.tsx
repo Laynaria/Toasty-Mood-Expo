26a466f565b9ce8a5924397b5bf472b53576ba00
Laynaria <pro.laynaria@outlook.fr>
2024-08-27 11:47:55 +0200

    exported button from root layout, to apply theme color to it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,12 @@
-import { router, Slot } from "expo-router";
-import { Image, Pressable, StyleSheet } from "react-native";
+import { Slot } from "expo-router";
 import { ThemeColorContextProvider } from "../contexts/ThemeColorContext";
-
-const backIcon = require("../assets/icons/back.png");
+import BackArrow from "../components/BackArrow";
 
 export default function RootLayout() {
   return (
     <ThemeColorContextProvider>
-      <Pressable onPress={router.back} style={styles.backButton}>
-        <Image source={backIcon} style={styles.img} />
-      </Pressable>
+      <BackArrow />
       <Slot />
     </ThemeColorContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  backButton: {
-    position: "absolute",
-    top: 40,
-    left: 17,
-    padding: 15,
-    zIndex: 1,
-    borderRadius: 15,
-  },
-  img: {
-    height: 20,
-    width: 15,
-  },
-});
---
+++ components/BackArrow.tsx
@@ -0,0 +1,33 @@
+import { router } from "expo-router";
+import { useContext } from "react";
+import { Image, Pressable, StyleSheet } from "react-native";
+import { ThemeColorContext } from "../contexts/ThemeColorContext";
+
+const backIcon = require("../assets/icons/back.png");
+
+export default function BackArrow() {
+  const { selectedTheme } = useContext(ThemeColorContext);
+  return (
+    <Pressable onPress={router.back} style={styles.backButton}>
+      <Image
+        source={backIcon}
+        style={[styles.img, { tintColor: selectedTheme.secondary }]}
+      />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  backButton: {
+    position: "absolute",
+    top: 40,
+    left: 17,
+    padding: 15,
+    zIndex: 1,
+    borderRadius: 15,
+  },
+  img: {
+    height: 20,
+    width: 15,
+  },
+});
