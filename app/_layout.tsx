58a2743795a68c4aab7bfb4881a09691edc8594e
Laynaria <pro.laynaria@outlook.fr>
2024-10-07 21:01:18 +0200

    first implementation of dark theme in layout, just using phone preferences for now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,23 @@ import { Slot } from "expo-router";
 import { ThemeColorContextProvider } from "../contexts/ThemeColorContext";
 import BackArrow from "../components/BackArrow";
 
+import { useColorScheme, View } from "react-native";
+
 export default function RootLayout() {
+  let colorScheme = useColorScheme();
+
   return (
     <ThemeColorContextProvider>
-      <BackArrow />
-      <Slot />
+      <View
+        style={{
+          display: "flex",
+          height: "100%",
+          backgroundColor: colorScheme === "light" ? "#FFFFFF" : "#220304",
+        }}
+      >
+        <BackArrow />
+        <Slot />
+      </View>
     </ThemeColorContextProvider>
   );
 }
