c5507c2ce4f339228f23db0253f529e5c0efbe6b
Laynaria <pro.laynaria@outlook.fr>
2025-01-10 15:34:38 +0100

    added jam days version assets and included them in toasts service array

--- services/toasts.ts
+++ services/toasts.ts
@@ -1,26 +1,34 @@
 const toastOkay = require("../assets/toasts/toast-okay.png");
+const jamToastOkay = require("../assets/toasts/toast-okay-jam.png");
+
 const toastVeryBad = require("../assets/toasts/toast-very-bad.png");
+const jamToastVeryBad = require("../assets/toasts/toast-very-bad-jam.png");
 
 const basicMoods = [
   {
     id: 1,
     img: toastOkay,
+    jamImg: jamToastOkay,
   },
   {
     id: 2,
     img: toastOkay,
+    jamImg: jamToastOkay,
   },
   {
     id: 3,
     img: toastOkay,
+    jamImg: jamToastOkay,
   },
   {
     id: 4,
     img: toastVeryBad,
+    jamImg: jamToastVeryBad,
   },
   {
     id: 5,
     img: toastVeryBad,
+    jamImg: jamToastVeryBad,
   },
 ];
 
