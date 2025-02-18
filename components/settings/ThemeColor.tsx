db443b5902770f7e8c21ff623d59d5dd7d7a1df4
Laynaria <pro.laynaria@outlook.fr>
2025-02-18 16:55:36 +0100

    removed container style variable from ThemeColor component now that it's not used anymore

--- components/settings/ThemeColor.tsx
+++ components/settings/ThemeColor.tsx
@@ -36,23 +36,6 @@ export default function ThemeColor() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "space-around",
-    rowGap: 24,
-    width: "90%",
-    paddingHorizontal: 16,
-    paddingTop: 5,
-    paddingBottom: 30,
-    borderRadius: 16,
-  },
-  text: {
-    fontWeight: "bold",
-    width: "100%",
-  },
   button: {
     borderRadius: 60,
     borderWidth: 2,
