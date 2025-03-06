274853a822434b0000e3c923e8a6fd5500960643
Laynaria <pro.laynaria@outlook.fr>
2025-03-06 19:01:25 +0100

    improved backArrow test

--- __tests__/components/layout/BackArrow.test.tsx
+++ __tests__/components/layout/BackArrow.test.tsx
@@ -1,6 +1,8 @@
 import { useGlobalSearchParams } from "expo-router";
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import BackArrow from "@/components/layout/BackArrow";
+import { act } from "react";
+import { handlePreviousPageNavigation } from "@/services/layoutService";
 
 jest.mock("expo-router", () => ({
   useGlobalSearchParams: jest.fn(),
@@ -12,10 +14,23 @@ jest.mock("expo-router", () => ({
   index: 30,
 });
 
+jest.mock("@/services/layoutService", () => ({
+  handlePreviousPageNavigation: jest.fn(),
+}));
+
 describe("<BackArrow />", () => {
   test("Check if BackArrow component contain the backarrow picture", () => {
     const { getByLabelText } = render(<BackArrow />);
 
     getByLabelText("Previous page icon");
+  });
+
+  test("Check if pressing button works and launch a mocked function 1 time", () => {
+    const { getByLabelText } = render(<BackArrow />);
+
+    const button = getByLabelText("Previous page icon");
+    act(() => fireEvent.press(button));
+
+    expect(handlePreviousPageNavigation).toHaveBeenCalledTimes(1);
   });
 });
