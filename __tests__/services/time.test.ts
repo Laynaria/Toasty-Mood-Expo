a033a0fb9350664e68d5c50dce2cb18a35a6250d
Laynaria <pro.laynaria@outlook.fr>
2025-03-11 21:23:08 +0100

    Added a test suite for daysInMonth function in time.ts test suite

--- __tests__/services/time.test.ts
+++ __tests__/services/time.test.ts
@@ -1,7 +1,7 @@
-import { getDaysName } from "../../services/time";
+import { daysInMonth, getDaysName } from "../../services/time";
 
 describe("time.ts test suite", () => {
-  describe("getDaysName function", () => {
+  describe("getDaysName function test suite", () => {
     test("Checks that getDayName properly return Thu for 16 May 2024", () => {
       expect(getDaysName("2024", "May", 16)).toBe("Thu");
     });
@@ -12,6 +12,20 @@ describe("time.ts test suite", () => {
 
     test("Checks that getDayName properly doesn't return Mon for 9 April 2025", () => {
       expect(getDaysName("2025", "April", 9)).not.toBe("Mon");
+    });
+  });
+
+  describe("daysInMonth function test suite", () => {
+    test("Checks that daysInMonth return 31 for May 2024", () => {
+      expect(daysInMonth("2024", "May")).toBe(31);
+    });
+
+    test("Checks that daysInMonth return 29 for February 2024", () => {
+      expect(daysInMonth("2024", "February")).toBe(29);
+    });
+
+    test("Checks that daysInMonth return 29 for February 2025", () => {
+      expect(daysInMonth("2025", "February")).toBe(28);
     });
   });
 });
