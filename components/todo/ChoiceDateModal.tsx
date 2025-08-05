e8ae99703c6d3150c6ae0424c4725ae00c11845a
Laynaria <pro.laynaria@outlook.fr>
2025-08-05 18:51:32 +0200

    removed placement issue for monthYearContainer class

--- components/todo/ChoiceDateModal.tsx
+++ components/todo/ChoiceDateModal.tsx
@@ -230,7 +230,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "80%",
   },
   daysContainer: {
     flexDirection: "row",
